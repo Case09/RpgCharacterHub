69a0c4d4a3d619c09f3fc01cf43e0169d8990255
Filip Uzunovic <filuzi77@gmail.com>
2017-11-28 22:41:31 +0100

    Nested routing pain with react-router v4

--- src/app.js
+++ src/app.js
@@ -6,21 +6,20 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store/store";
-import { database, firebaseAuth } from './config/database';
-import { PrivateRoute, PublicRoute } from './helpers/routes';
+import { database, firebaseAuth } from "./config/database";
+import { PrivateRoute, PublicRoute } from "./helpers/routes";
 
 import Home from "./components/containers/Home";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
 class Main extends React.Component {
-
 	constructor(props) {
 		super(props);
 		this.state = {
 			authed: false,
 			loading: true
-		}
+		};
 	}
 
 	componentDidMount() {
@@ -48,19 +47,22 @@ class Main extends React.Component {
 		return (
 			<Provider store={store}>
 				<Router>
-					<Switch>
-						<Route path="/" exact component={Home} />
-						<PublicRoute
-							authed={this.state.authed}
-							path="/login"
-							component={Login}
-						/>
-						<PrivateRoute
-							authed={this.state.authed}
-							path="/dashboard"
-							component={Dashboard}
-						/>
-					</Switch>
+					<div>
+						{ /* Not setting path makes this render for every /** path, makes nested routing basically */ }
+						<Route exact component={Home} />
+						<Switch>
+							<PublicRoute
+								authed={this.state.authed}
+								path="/login"
+								component={Login}
+							/>
+							<PrivateRoute
+								authed={this.state.authed}
+								path="/dashboard"
+								component={Dashboard}
+							/>
+						</Switch>
+					</div>
 				</Router>
 			</Provider>
 		);
