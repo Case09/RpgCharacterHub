c949d086fad34ce137cb1c5e87fc570865334595
Filip Uzunovic <filuzi77@gmail.com>
2017-12-15 23:25:21 +0100

    Removed comments

--- src/actions/sheets.js
+++ src/actions/sheets.js
@@ -2,23 +2,6 @@ import ActionTypes from '../constants/action_types';
 import { database, firebaseAuth } from '../config/database';
 import { create } from '../helpers/sheets';
 
-// const data = {
-//     sheets: {
-//         sheetGuid: {
-//             creator: 'user1',
-//             isPrivate: false,
-//             data: {...data}
-//         }
-//     },
-//     favorites: {
-//         user1: {
-//             sheets: [
-//                 sheetData1, sheetData2
-//             ]
-//         }
-//     }
-// }
-
 export function createSheet(uid, data) {
     return dispatch => {
         create(uid, data)
